ef3d253443764815b1595ea51f1a9f888f616e79
Aldo Suhartono Putra <aldhosutra@gmail.com>
2020-12-28 13:37:42 +0700

    initial commit

---
+++ lisk-explorer/config.js
@@ -0,0 +1,117 @@
+/*
+ * LiskHQ/lisk-explorer
+ * Copyright © 2018 Lisk Foundation
+ *
+ * See the LICENSE file at the top-level directory of this distribution
+ * for licensing information.
+ *
+ * Unless otherwise agreed in a custom licensing agreement with the Lisk Foundation,
+ * no part of this software, including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in the
+ * LICENSE file.
+ *
+ * Removal or modification of this copyright notice is prohibited.
+ *
+ */
+const config = require("./config.global");
+
+/**
+ * Frontend UI Message
+ */
+config.uiMessage.text = process.env.UI_MESSAGE || "";
+config.uiMessage.start = process.env.UI_MESSAGE_START || ""; // optional, ISO Date-Time ex. '2018-07-11T15:01:00+02:00'
+config.uiMessage.end = process.env.UI_MESSAGE_END || ""; // optional, ISO Date-Time ex. '2018-07-11T15:01:00+02:00'
+
+/**
+ * CONFIGURATION
+ */
+config.host = "0.0.0.0"; // Interface to listen on, 0.0.0.0 to listen on all available
+config.port = 6040; // Port to listen on
+
+/**
+ * LISK node
+ */
+config.lisk.host = process.env.LISK_HOST || "127.0.0.1";
+config.lisk.port = process.env.LISK_PORT || 4000;
+config.lisk.apiPath = "/api";
+
+/**
+ * FreeGeoIP server
+ */
+config.freegeoip.host = process.env.FREEGEOIP_HOST || "127.0.0.1";
+config.freegeoip.port = process.env.FREEGEOIP_PORT || 8080;
+
+/**
+ * Redis server
+ */
+config.redis.host = process.env.REDIS_HOST || "127.0.0.1";
+config.redis.port = process.env.REDIS_PORT || 6379;
+config.redis.db = process.env.REDIS_DB || 0;
+config.redis.password = "";
+
+// Time in seconds to store cache in Redis
+config.cacheTTL = 20;
+
+// Collect logs (true - enabled, false - disabled)
+config.log.enabled = true;
+// Output for logs - can be device file or ordinary path
+config.log.output = ["/dev/stdout", "./logs/explorer.log"];
+// Log level - (trace, debug, info, warn, error)
+config.log.level = "info";
+
+/**
+ * Header price tickers, Currency switcher
+ */
+// Exchange rates support (true - enabled, false - disabled)
+config.exchangeRates.enabled = false;
+// Interval in ms for checking exchange rates (default: 30 seconds)
+config.exchangeRates.updateInterval = 30000;
+
+// Configuration for different currency pairs, set false to disable pair
+// LSK/BTC pair, supported: poloniex
+config.exchangeRates.exchanges.LSK.BTC = "poloniex";
+// BTC/USD pair, supported: bitfinex, bitstamp, btce
+config.exchangeRates.exchanges.BTC.USD = "bitfinex";
+// BTC/EUR pair, supported: bitstamp, bitmarket
+config.exchangeRates.exchanges.BTC.EUR = "bitstamp";
+// BTC/RUB pair, supported: btce, exmo
+config.exchangeRates.exchanges.BTC.RUB = "btce";
+// BTC/PLN pair, supported: bitmarket, bitbay
+config.exchangeRates.exchanges.BTC.PLN = "bitmarket";
+// BTC/JPY pair, supported: coincheck
+config.exchangeRates.exchanges.BTC.JPY = "coincheck";
+// BTC/GBP pair, supported: coinbase
+config.exchangeRates.exchanges.BTC.GBP = "coinbase";
+// BTC/CNY pair, supported: 1btcxe, coinmarketcap
+config.exchangeRates.exchanges.BTC.CNY = "1btcxe";
+
+/**
+ * Market watcher
+ */
+// Market watcher support (true - enabled, false - disabled)
+config.marketWatcher.enabled = false;
+// Poloniex exchange support (true - enabled, false - disabled)
+config.marketWatcher.exchanges.poloniex = true;
+// Bittrex exchange support (true - enabled, false - disabled);
+config.marketWatcher.exchanges.bittrex = true;
+// Interval in ms for updating candlestick data (default: 30 seconds)
+config.marketWatcher.candles.updateInterval = 30000;
+// Build candles based on trades form last 30 days
+config.marketWatcher.candles.poloniex.buildTimeframe = 60 * 60 * 24 * 30;
+// Interval in ms for updating order book data (default: 15 seconds)
+config.marketWatcher.orders.updateInterval = 15000;
+
+/**
+ * Cache delegate info in order to replace address by username
+ */
+// Delegate caching support (true - enabled, false - disabled)
+config.cacheDelegateAddress.enabled = true;
+// Interval in ms for checking new delegates registration (default: 60 seconds)
+config.cacheDelegateAddress.updateInterval = 60000;
+
+/*
+ * Known accounts can be loaded from the url below
+ */
+config.knownAccountsUrl = "";
+
+module.exports = config;
---
+++ lisk-faucet/api/lisk.js
@@ -0,0 +1,225 @@
+const request = require("request"),
+  async = require("async"),
+  redis = require("./redis"),
+  simple_recaptcha = require("simple-recaptcha"),
+  packageJson = require("../package.json");
+
+const { APIClient, transactions, cryptography } = require("lisk-elements");
+
+const getApiClient = (app) => {
+  return new APIClient([app.locals.liskUrl], {
+    client: {
+      name: "Lisk Faucet",
+      version: packageJson.version,
+    },
+    nethash: app.locals.nethash,
+  });
+};
+
+module.exports = function (app) {
+  app.get("/api/getBase", function (req, res) {
+    const apiClient = getApiClient(app);
+
+    async.series(
+      [
+        function (cb) {
+          apiClient.accounts
+            .get({ address: app.locals.address })
+            .then((accounts) => {
+              cb(null, accounts.data[0].balance);
+            })
+            .catch((err) => {
+              cb("Failed to get faucet balance");
+            });
+        },
+        function (cb) {
+          apiClient.node
+            .getConstants()
+            .then((constants) => {
+              cb(null, constants.data.fees.send);
+            })
+            .catch((err) => {
+              cb("Failed to establish transaction fee");
+            });
+        },
+      ],
+      function (error, result) {
+        if (error) {
+          return res.json({ success: false, error: error });
+        } else {
+          var balance = result[0],
+            fee = result[2],
+            hasBalance = false;
+
+          if (
+            app.locals.amountToSend * req.fixedPoint +
+              ((app.locals.amountToSend * req.fixedPoint) / 100) * fee <=
+            balance
+          ) {
+            hasBalance = true;
+          }
+
+          return res.json({
+            success: true,
+            captchaKey: app.locals.captcha.publicKey,
+            balance: balance / req.fixedPoint,
+            fee: fee,
+            hasBalance: hasBalance,
+            amount: app.locals.amountToSend,
+            donation_address: app.locals.address,
+            network: app.locals.network_name,
+            explorerUrl: app.locals.explorerUrl,
+          });
+        }
+      }
+    );
+  });
+
+  app.post("/api/sendLisk", function (req, res) {
+    var error = null,
+      address = req.body.address,
+      captcha_response = req.body.captcha,
+      ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+
+    if (!address) {
+      error = "Missing Lisk ID";
+    }
+
+    if (!captcha_response) {
+      error = "Captcha validation failed, please try again";
+    }
+
+    if (address) {
+      address = address.trim();
+
+      if (
+        address.indexOf("L") != address.length - 1 &&
+        address.indexOf("D") != address.length - 1
+      ) {
+        error = "Invalid Lisk ID";
+      }
+
+      var num = address.substring(0, address.length - 1);
+      if (isNaN(num)) {
+        error = "Invalid Lisk ID";
+      }
+    }
+
+    if (error) {
+      return res.json({ success: false, error: error });
+    }
+
+    var parallel = {
+      authenticateIP: function (cb) {
+        redis.getClient().get(ip, function (error, value) {
+          if (error) {
+            return cb("Failed to authenticate IP address");
+          } else if (value) {
+            return cb("This IP address has already received LSK");
+          } else {
+            return cb(null);
+          }
+        });
+      },
+      authenticateAddress: function (cb) {
+        redis.getClient().get(address, function (error, value) {
+          if (error) {
+            return cb("Failed to authenticate Lisk ID");
+          } else if (value) {
+            return cb("This account has already received LSK");
+          } else {
+            return cb(null);
+          }
+        });
+      },
+    };
+
+    var series = {
+      validateCaptcha: function (cb) {
+        simple_recaptcha(
+          app.locals.captcha.privateKey,
+          ip,
+          captcha_response,
+          function (error) {
+            if (error) {
+              return cb("Captcha validation failed, please try again");
+            } else {
+              return cb(null);
+            }
+          }
+        );
+      },
+      cacheIP: function (cb) {
+        redis
+          .getClient()
+          .set(ip, ip, "EX", app.locals.cacheTTL, function (error) {
+            if (error) {
+              return cb("Failed to cache IP address");
+            } else {
+              return cb(null);
+            }
+          });
+      },
+      cacheAddress: function (cb) {
+        redis
+          .getClient()
+          .set(address, address, "EX", app.locals.cacheTTL, function (error) {
+            if (error) {
+              return cb("Failed to cache Lisk ID");
+            } else {
+              return cb(null);
+            }
+          });
+      },
+      sendTransaction: function (cb) {
+        const amount = app.locals.amountToSend * req.fixedPoint;
+        const networkIdentifier = cryptography.getNetworkIdentifier(
+          app.locals.nethash,
+          app.locals.network_name
+        );
+        const transaction = transactions.transfer({
+          networkIdentifier,
+          recipientId: address,
+          amount: String(amount),
+          passphrase: app.locals.passphrase,
+        });
+
+        const apiClient = getApiClient(app);
+        apiClient.transactions
+          .broadcast(transaction)
+          .then((transaction) => {
+            return cb(null, transaction.data);
+          })
+          .catch((err) => {
+            console.log(err.message);
+            return cb("Failed to send transaction");
+          });
+      },
+    };
+
+    async.parallel(
+      [parallel.authenticateIP, parallel.authenticateAddress],
+      function (error, values) {
+        if (error) {
+          return res.json({ success: false, error: error });
+        } else {
+          async.series(
+            [
+              series.validateCaptcha,
+              series.cacheIP,
+              series.cacheAddress,
+              series.sendTransaction,
+            ],
+            function (error, results) {
+              if (error) {
+                return res.json({ success: false, error: error });
+              } else {
+                return res.json({ success: true, txId: "unknown" });
+              }
+            }
+          );
+        }
+      }
+    );
+  });
+};
